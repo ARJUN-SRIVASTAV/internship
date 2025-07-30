e166ceef97d59f9bc022937e0fd49be6ad09dfe7
ARJUN-SRIVASTAV <164508907+ARJUN-SRIVASTAV@users.noreply.github.com>
2025-07-30 19:08:36 +0530

    Create logfile.js

---
+++ logfile.js
@@ -0,0 +1,24 @@
+const http =require('http');
+const fs=require("fs");
+const url=require("url");
+
+const myServer=http.createServer((req,res)=>{
+    if(req.url=== '/favicon.ico') return res.end();
+    const log=`${Date.now()}: ${req.url} new request receive\n`;
+    const myurl= url.parse(req.url);
+    console.log(myurl);
+    fs.appendFile('log.txt',log,(err,data)=>{
+        switch(req.url)
+        {
+            case '/': res.end("home page");
+            break;
+            case '/about':res.end("about page");
+            break;
+            default:res.end("404");
+        }
+
+    })
+    // console.log("new record");
+    
+});
+myServer.listen(4500,()=>{console.log("started")});
